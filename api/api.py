9590da98ee88b190e0f67f6c49b2a1f3176cde66
ORuan <ruanpablomp@gmail.com>
2021-01-16 12:55:36 -0300

    add sintaxe"s helper

--- api/api.py
+++ api/api.py
@@ -5,6 +5,8 @@ from selenium_controller import SearchCorreios
 
 app = flask.Flask(__name__)
 print('[*]:É provavel que um diretório driver seja criado, não se precoupe\n')
+print('[*]:Para usar a API digite: no browser\n')
+print('http:localhost:5000/?code={code}\n')
 
 @app.route('/', methods=['GET'])
 def query_bot():
